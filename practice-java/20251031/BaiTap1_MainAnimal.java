d71d7c747ce4b2cdec18f3aae1c4d4c711477844
TrangMai288 <trangmai828@gmail.com>
2025-10-31 17:31:10 +0700

    cập nhật thêm nội dung mới

--- practice-java/20251031/BaiTap1_MainAnimal.java
+++ practice-java/20251031/BaiTap1_MainAnimal.java
@@ -1,3 +1,14 @@
+/*
+👉 Đề bài:
+Viết chương trình Java mô phỏng đa hình với các lớp sau:
+Animal: có phương thức makeSound() in ra "Animal sound".
+Dog: kế thừa Animal, ghi đè makeSound() in "Woof!".
+Cat: kế thừa Animal, ghi đè makeSound() in "Meow!".
+Trong main():
+Tạo Dog và Cat riêng lẻ, gọi makeSound().
+Sau đó tạo một mảng Animal[] chứa Dog và Cat, duyệt qua và gọi makeSound() của từng phần tử.
+ */
+
 import java.util.ArrayList;
 
 public class BaiTap1_MainAnimal {
